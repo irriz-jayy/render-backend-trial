c2e3cfe3c2b08893c210b8a1c2b3ff0baf7b3d6c
irriz-jayy <jayfelix789@gmail.com>
2023-06-02 16:47:26 +0300

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+    def index
+        render json: User.all
+    end
+
+    def show
+        user= User.find(params[:id])
+        render json: user, status: :ok
+    end
+end
